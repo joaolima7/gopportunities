8176d212d140289214cf2eb4caaf0c2f09544120
joaolima7 <jcgam3r69@gmail.com>
2025-01-09 21:49:12 -0300

    feat: adicionar configuração e logger, e definir estrutura de abertura

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/joaolima7/gopportunities.git/router"
+import (
+	"github.com/joaolima7/gopportunities.git/config"
+	"github.com/joaolima7/gopportunities.git/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		panic(err)
+	}
 	router.Initialize()
 }
